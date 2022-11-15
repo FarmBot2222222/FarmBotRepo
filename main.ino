161d218ebedae491e75f2f9c3720a70478a7fd3e
FarmBot2222222 <117803563+FarmBot2222222@users.noreply.github.com>
2022-11-15 08:47:12 +0400

    Update main.ino

--- main.ino
+++ main.ino
@@ -2,52 +2,57 @@ int motor_L = 12;
 int motor_R = 13;
 
 void setup() {
-    pinMode(motor_L, OUTPUT);
-    pinMode(motor_R, OUTPUT);
+  Serial.begin(9600);
+  pinMode(motor_L, OUTPUT);
+  pinMode(motor_R, OUTPUT);
 }
 
 void loop() {
-
+  Sequence1();
 }
 
 void MoveForward() {
-    digitalWrite(motor_L, HIGH);
-    digitalWrite(motor_R, HIGH);
+  digitalWrite(motor_L, HIGH);
+  digitalWrite(motor_R, HIGH);
 }
 
 void StopMotors() {
-    digitalWrite(motor_L, LOW);
-    digitalWrite(motor_R, LOW);
+  digitalWrite(motor_L, LOW);
+  digitalWrite(motor_R, LOW);
 }
 
 void MoveLeft() {
-    digitalWrite(motor_L, LOW);
-    digitalWrite(motor_R, HIGH);
-    delay(500);
-    StopMotors();
+  digitalWrite(motor_L, LOW);
+  digitalWrite(motor_R, HIGH);
+  delay(2000);
+  StopMotors();
 }
 
 void MoveRight() {
-    digitalWrite(motor_L, HIGH);
-    digitalWrite(motor_R, LOW);
-    delay(500);
-    StopMotors();
+  digitalWrite(motor_L, HIGH);
+  digitalWrite(motor_R, LOW);
+  delay(2000);
+  StopMotors();
 }
 
 void Sequence1() {
-    
-    MoveForward();
-    delay(2000);
+  
+  MoveForward();
+  delay(8000);
 
-    MoveLeft();
-    MoveForward();
-    delay(2000);
+  MoveRight();
+  MoveForward();
+  delay(8000);
 
-    MoveLeft();
-    MoveForward();
-    delay(2000);
+  MoveRight();
+  MoveForward();
+  delay(8000);
 
-    MoveLeft();
-    MoveForward();
-    delay(2000);
+  MoveRight();
+  MoveForward();
+  delay(8000);
+
+  StopMotors();
+  delay(5000);
+
 }
